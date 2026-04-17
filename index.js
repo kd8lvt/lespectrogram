70f43f973643f729a2ce68a8a70e484a09889761
kd8lvt <kd8lvt@kd8lvt.com>
2026-04-17 14:50:24 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let spectrogram = null;
+
+async function init() {
+    const audioCtx = new AudioContext();
+    document.getElementById("start").style.visibility = "hidden";
+
+    navigator.mediaDevices.getUserMedia({video:false,audio:true}).then(stream=>{
+        const analyser = audioCtx.createAnalyser()
+        const src = audioCtx.createMediaStreamSource(stream);
+        src.connect(analyser)
+        
+        const frqBuf = new Uint8Array(analyser.frequencyBinCount);
+        const wfNumPts = 50*analyser.frequencyBinCount/256;
+        const wfBufAry = {buffer:frqBuf};
+
+        const wf = new Waterfall(wfBufAry,wfNumPts*3,wfNumPts*8,"right",{onscreenParentId:"root"});
+
+        function draw() {
+            analyser.getByteFrequencyData(frqBuf,0);
+
+            requestAnimationFrame(draw)
+        }
+
+        draw();
+        wf.start()
+    });
+}
+
+window.addEventListener('DOMContentLoaded',()=>{
+    document.getElementById("start").addEventListener('click',init);
+})
