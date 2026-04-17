64d0f070889cebd60bd1a5d31bd83a6296ad870e
kd8lvt <kd8lvt@kd8lvt.com>
2026-04-17 15:50:18 -0400

    pausing and resetting

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let spectrogram = null;
-let scaleType = "MEL";
-
+let spectrogram=null;
+let paused=false;
 
 async function init() {
     const audioCtx = new AudioContext();
-    document.getElementById("start").style.visibility = "hidden";
+    document.getElementById("start").style.display = "none";
+    document.getElementById("pause").style.display = "";
+    document.getElementById("reset").style.display = "";
 
     navigator.mediaDevices.getUserMedia({video:false,audio:true}).then(stream=>{
         const analyser = audioCtx.createAnalyser()
@@ -15,21 +16,26 @@ async function init() {
         const wfNumPts = 50*analyser.frequencyBinCount/256;
         const wfBufAry = {buffer:frqBuf};
 
-        const wf = new Waterfall(wfBufAry,wfNumPts*3,wfNumPts*8,"right",{onscreenParentId:"root"});
+        spectrogram = new Waterfall(wfBufAry,wfNumPts*3,wfNumPts*8,"right",{onscreenParentId:"root"});
 
         function draw() {
             analyser.getByteFrequencyData(frqBuf,0);
-            
-
 
             requestAnimationFrame(draw)
         }
 
         draw();
-        wf.start()
+        spectrogram.start()
+        spectrogram.clear()
     });
 }
 
 window.addEventListener('DOMContentLoaded',()=>{
     document.getElementById("start").addEventListener('click',init);
+    document.getElementById("pause").addEventListener('click',()=>{
+        paused=!paused
+        if (paused) return spectrogram.stop();
+        spectrogram.start()
+    });
+    document.getElementById("reset").addEventListener('click',()=>spectrogram.clear());
 })
